9f02c4c143843d94b47e738a6ae7ba6f8517904b
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-02-01 14:26:01 +0100

    :sparkles: feat(users): POST an id to get full information about user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,13 +8,28 @@ router.get('/', (req, res) => {
     .find({}, { projection: { password: 0 } })
     .toArray()
     .then((results) => {
-      res.json(results);
+      if (results) {
+        res.json(results);
+      } else {
+        res.status(404).json({ error: 'There are no users in the database!' });
+      }
     });
 });
 
 /* POST one user id / return all info of user */
 router.post('/', (req, res) => {
-  res.send('respond with a resource');
+  const userId = req.body.id;
+  console.log(userId);
+  req.app.locals.db
+    .collection('users')
+    .findOne({ id: userId })
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ error: 'User does not excist!' });
+      }
+    });
 });
 
 /* POST create a new user */
