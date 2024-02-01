9dfa83230fd03b801ee5e7714c4ef8cfedfc26af
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-02-01 13:20:05 +0100

    :sparkle: feat(app): connected db to project

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,17 @@ const ordersRouter = require('./routes/orders');
 
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017', {
+  useUnifiedTopology: true,
+}).then((client) => {
+  console.log('Databasen is up and running! Och vi har kontakt!');
+
+  const db = client.db('oskar-landstrom');
+  app.locals.db = db;
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
