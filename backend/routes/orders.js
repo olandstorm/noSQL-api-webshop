34b32ceec04633cea9b63e24c669f20ce57bbfa9
olandstorm <100365091+olandstorm@users.noreply.github.com>
2024-02-02 13:48:44 +0100

    :sparkles: feat(orders): get all orders is now possible

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,11 +3,21 @@ const router = express.Router();
 
 /* GET all orders */
 router.get('/all', (req, res) => {
-  res.send('respond with a resource');
+  req.app.locals.db
+    .collection('orders')
+    .find()
+    .toArray()
+    .then((results) => {
+      if (results) {
+        res.json(results);
+      } else {
+        res.status(404).json({ error: 'There are no orders in the database!' });
+      }
+    });
 });
 
 /* POST a new order for a user */
-router.post('/all', (req, res) => {
+router.post('/add', (req, res) => {
   res.send('respond with a resource');
 });
 
